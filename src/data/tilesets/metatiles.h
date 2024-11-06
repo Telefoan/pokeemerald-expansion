27ab3cb6095cc27d898a6f2a1ef9328ec98db0c3
Telefoan <155188161+Telefoan@users.noreply.github.com>
2024-11-06 11:42:12 -0500

    new palettes linked

--- src/data/tilesets/metatiles.h
+++ src/data/tilesets/metatiles.h
@@ -207,3 +207,12 @@ const u16 gMetatileAttributes_MysteryEventsHouse[] = INCBIN_U16("data/tilesets/s
 
 const u16 gMetatiles_UnionRoom[] = INCBIN_U16("data/tilesets/secondary/union_room/metatiles.bin");
 const u16 gMetatileAttributes_UnionRoom[] = INCBIN_U16("data/tilesets/secondary/union_room/metatile_attributes.bin");
+
+const u16 gMetatiles_BeachTown[] = INCBIN_U16("data/tilesets/secondary/beach_town/metatiles.bin");
+const u16 gMetatileAttributes_BeachTown[] = INCBIN_U16("data/tilesets/secondary/beach_town/metatile_attributes.bin");
+
+const u16 gMetatiles_beach[] = INCBIN_U16("data/tilesets/secondary/beach/metatiles.bin");
+const u16 gMetatileAttributes_beach[] = INCBIN_U16("data/tilesets/secondary/beach/metatile_attributes.bin");
+
+const u16 gMetatiles_copypalette[] = INCBIN_U16("data/tilesets/primary/copypalette/metatiles.bin");
+const u16 gMetatileAttributes_copypalette[] = INCBIN_U16("data/tilesets/primary/copypalette/metatile_attributes.bin");
