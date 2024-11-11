2ec23ff275ff003da6416442d3c1649ec2fa0e1d
Telefoan <155188161+Telefoan@users.noreply.github.com>
2024-11-11 09:41:41 -0500

    oops

--- src/data/tilesets/graphics.h
+++ src/data/tilesets/graphics.h
@@ -45,22 +45,22 @@ const u32 gTilesetTiles_copypalette[] = INCBIN_U32("data/tilesets/primary/copypa
 
 const u16 gTilesetPalettes_copypalette[][16] =
 {
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/00.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/01.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/02.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/03.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/04.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/05.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/06.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/07.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/08.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/09.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/10.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/11.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/12.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/13.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/14.gbapal"),
-    INCBIN_U16("data/tilesets/secondary/copypalette/palettes/15.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/00.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/01.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/02.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/03.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/04.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/05.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/06.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/07.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/08.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/09.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/10.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/11.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/12.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/13.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/14.gbapal"),
+    INCBIN_U16("data/tilesets/primary/copypalette/palettes/15.gbapal"),
 };
 
 const u32 gTilesetTiles_Petalburg[] = INCBIN_U32("data/tilesets/secondary/petalburg/tiles.4bpp.lz");
